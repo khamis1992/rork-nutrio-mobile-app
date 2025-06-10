da30d57b2a647ef774cd8a11c53c96acd71bb0a8
khamis1992 <khamis-1992@hotmail.com>
2025-06-10 18:54:19 +0300

    Fix fetchPriority prop casing in remaining Image components

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -23,7 +23,6 @@ export default function WelcomeScreen() {
           <Image
             source={{ uri: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80' }}
             style={styles.logoBackground}
-            web={{ fetchpriority: 'auto' }}
           />
           <View style={styles.logoOverlay} />
           <Text style={styles.logo}>NUTRIO</Text>
